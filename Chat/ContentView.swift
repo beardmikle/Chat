ea27f393fcdbd84359c0b8ee0f142e20da9eb086
Mikle <beardmikle@gmail.com>
2023-05-11 02:14:15 +0300

    first step

--- Chat/ContentView.swift
+++ Chat/ContentView.swift
@@ -8,14 +8,80 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var isLoginMode = false
+    @State var email = ""
+    @State var password = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Let`s doing something")
+        NavigationView {
+            ScrollView {
+                
+                VStack(spacing: 18) {
+                    Picker(selection: $isLoginMode, label: Text("Picker here"))
+                    {
+                        Text("Login")
+                            .tag(true)
+                        Text("Create Account")
+                            .tag(false)
+                    }.pickerStyle(SegmentedPickerStyle())
+                        .padding()
+                    
+
+                    if !isLoginMode {
+                        Button {
+                            
+                        } label: {
+                            Image(systemName: "person")
+                                .font(.system(size: 69))
+                                .padding()
+                        }
+                        
+
+                    }
+                    Group {
+                        TextField("Email", text: $email)
+                            .keyboardType(.emailAddress)
+                            .autocapitalization(.none)
+
+                        SecureField("Password", text: $password)
+                    }
+                    .padding(13)
+                    .background(Color.white)
+  
+                    
+                    Button {
+                        
+                    } label: {
+                        HStack
+                        {
+                            Spacer()
+                            Text(isLoginMode ? "Log In" : "Create Account")
+                                .foregroundColor(.white)
+                                .padding(.vertical, 10)
+                                .font(.system(size: 16, weight: .semibold))
+                            Spacer()
+                        }.background(Color.blue)
+                        
+                    }
+
+                }
+                .padding()
+                
+            }
+            .navigationTitle(isLoginMode ? "Log In" : "Create Account")
+            .background(Color(.init(white: 0, alpha: 0.05))
+                .ignoresSafeArea())
+            
         }
-        .padding()
+  
+    }
+    
+    private func handleAction() {
+        if isLoginMode {
+            print("Should log in Firebase")
+        } else {
+            print("Reister a new account of Firebase")
+        }
     }
 }
 
