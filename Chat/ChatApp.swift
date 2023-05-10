5c91519dc99acef8dd7e77bc05b59f05386ceb93
Mikle <beardmikle@gmail.com>
2023-05-10 21:06:18 +0300

    Initial Commit

---
+++ Chat/ChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatApp.swift
+//  Chat
+//
+//  Created by beardmikle on 10.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct ChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
